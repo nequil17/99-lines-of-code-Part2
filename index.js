d73ba6038b89b72171d25785168bb0c6896e721a
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-06-19 17:30:40 -0700

    pt2

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ var friends = [ "Brian", "Kelsey", "Mike", "Lou", "Brittney" ];
 var button = document.getElementById("button");
 
 button.addEventListener('click', function() {
-    
     for(var i = 0; i < friends.length; i++) {
+            var friend = friends[i];
+
+    var people = document.createElement('div');
+    people.className = 'friend'
+    var names = document.createElement('h3');
+    var textName = document.createTextNode(friend);
+    names.appendChild(textName);
+    people.appendChild(names);
+    document.body.appendChild(people);
+    
     console.log (friends[i] + ':')
         for(var j = 99; j >= 1; j--) {
+            var lyrics;
             if (j > 2) {
-                console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file' );
+                lyrics = document.createTextNode([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friend+ ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file' );
             } else if (j == 2) {
-                console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' line of code in the file' ); 
+                lyrics = document.createTextNode([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friend+ ' strikes one out, clears it all out, ' + (j-1) + ' line of code in the file' ); 
             } else if (j = 1) {
-                console.log([j] + ' line of code in the file, ' + [j] + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file' );
-                }
+                lyrics = document.createTextNode([j] + ' line of code in the file, ' + [j] + ' line of code; ' + friend+ ' strikes one out, clears it all out, no more lines of code in the file' );
+            }
+            var music = document.createElement('p');
+            music.appendChild(lyrics);
+            people.appendChild(music);
             }
         }
     });
